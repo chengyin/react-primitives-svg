3b6bab1554d98aa7984a503e93ce567b10a4b1c3
Mathieu Dutour <mat.dutour@gmail.com>
2017-11-12 12:32:26 +0000

    add support for Sketch renderer

---
+++ src/core.sketch.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-named-as-default */
+
+import { Svg } from 'react-sketchapp';
+
+const {
+  Circle,
+  Ellipse,
+  G,
+  LinearGradient,
+  RadialGradient,
+  Line,
+  Path,
+  Polygon,
+  Polyline,
+  Rect,
+  Symbol,
+  Text,
+  Use,
+  Defs,
+  Stop,
+} = Svg
+
+module.exports = {
+  Svg,
+  Circle,
+  Ellipse,
+  G,
+  LinearGradient,
+  RadialGradient,
+  Line,
+  Path,
+  Polygon,
+  Polyline,
+  Rect,
+  Symbol,
+  Text,
+  Use,
+  Defs,
+  Stop,
+};
